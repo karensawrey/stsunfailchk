76262294b956bd7798dbcbbf30fd11e7cb635ffa
Karen Sawrey <krnswry@gmail.com>
2025-02-26 15:23:09 +0100

    Create hello-meowrld.sh
    Adds a cat-themed Hello World

---
+++ hello-meowrld.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Hello Meowrld
+
+echo "   /\_/\  "
+echo "  ( o.o ) "
+echo "   > ^ <  "
+echo ""
+echo " 🐾  Meow! Hello World! 🐾 "
+echo ""
+echo "  /\_/\  zzZ"
+echo " ( -.- )   "
+echo "  > - <    "
+
+echo "   /\_/\  "
+echo "  ( o.o ) "
+echo "   > ^ <  "
+echo ""
+echo " 🐾  Meow! Hello World! 🐾 "
+echo ""
+echo "  /\_/\  zzZ"
+echo " ( -.- )   "
+echo "  > - <    "
